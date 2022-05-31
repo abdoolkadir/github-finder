9dc597dab4f55366fc41d3695011e3b2b04a736a
Abdulkadir Adamu <adamuhammadabdulkadir@gmail.com>
2022-05-31 15:08:05 +0100

    alert component and get single user

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Alert from './components/layout/Alert';
 import Footer from './components/layout/Footer';
 import Navbar from './components/layout/Navbar';
 import Home from './pages/Home';
+import User from './pages/User';
 import About from './pages/About';
 import NotFound from './pages/NotFound';
 import { GithubProvider } from './context/github/GithubContext';
@@ -20,6 +21,7 @@ function App() {
               <Alert />
               <Routes>
                 <Route path="/" element={<Home />}></Route>
+                <Route path="/user/:login" element={<User />}></Route>
                 <Route path="/about" element={<About />}></Route>
                 <Route path="/*" element={<NotFound />}></Route>
               </Routes>
